2e2135b3df4ad3fa4c03ca97a59d65fc06b9cc11
prinzpiuz <prinzpiuz@gmail.com>
2020-02-13 18:48:26 +0530

    added settings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:msm/settings_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,10 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  void pressed() {
-    print("pressed");
-  }
-
   @override
   void initState() {
     // TODO: implement initState
@@ -25,51 +22,68 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     final title = 'Media Server Manager';
-    var headings = ['Upload', 'Server', 'Files', 'Settings'];
     return MaterialApp(
       title: title,
-      home: Scaffold(
-          backgroundColor: Colors.white,
-          body: new Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              new Flexible(
-                  child: GridView.count(
-                crossAxisCount: 2,
-                children: List.generate(4, (index) {
-                  return Container(
-                    margin: const EdgeInsets.all(5.0),
-                    child: RaisedButton(
-                        onPressed: pressed,
-                        color: Colors.green,
-                        child: Center(
-                            child: Text(headings[index],
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 30)))),
-                  );
-                }),
-              )),
-              new Row(
-                children: <Widget>[
-                  new Flexible(
-                    child: const ListTile(
-                      leading: Icon(Icons.computer, size: 50, color:Colors.green),
-                      title: Text('Druv 1.0')                      
-                      // subtitle: is_server_live(),
-                    ),
-                  ),
-                  new Flexible(
-                    child: const ListTile(
-                      leading: Icon(Icons.folder, size: 50),
-                      title: Text('344'),
-                      subtitle: Text('out of 500 GB'),
-                    ),
-                  )
-                ],
-              )
-            ],
-          )),
+      home: MyHome(),
     );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  void pressed(index, context) {
+    if (index == 3) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SettingsPage()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var headings = ['Upload', 'Server', 'Files', 'Settings'];
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Flexible(
+                child: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(4, (index) {
+                return Container(
+                  margin: const EdgeInsets.all(5.0),
+                  child: RaisedButton(
+                      onPressed: () => pressed(index, context),
+                      color: Colors.green,
+                      child: Center(
+                          child: Text(headings[index],
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 30)))),
+                );
+              }),
+            )),
+            new Row(
+              children: <Widget>[
+                new Flexible(
+                  child: const ListTile(
+                      leading:
+                          Icon(Icons.computer, size: 50, color: Colors.green),
+                      title: Text('Druv 1.0')
+                      // subtitle: is_server_live(),
+                      ),
+                ),
+                new Flexible(
+                  child: const ListTile(
+                    leading: Icon(Icons.folder, size: 50),
+                    title: Text('344'),
+                    subtitle: Text('out of 500 GB'),
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
---
+++ lib/settings_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => new _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text(
+            'Settings',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Theme(
+          data: new ThemeData(
+              primaryColor: Colors.green,
+              accentColor: Colors.orange,
+              hintColor: Colors.grey),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: new InputDecoration(
+                  labelText: "Enter Email",
+                  fillColor: Colors.green,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(25.0),
+                    borderSide: new BorderSide(),
+                  ),
+                  //fillColor: Colors.green
+                ),
+                validator: (val) {
+                  if (val.length == 0) {
+                    return "Email cannot be empty";
+                  } else {
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+                style: new TextStyle(
+                  fontFamily: "Poppins",
+                ),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: new InputDecoration(
+                  labelText: "Enter Email",
+                  fillColor: Colors.green,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(25.0),
+                    borderSide: new BorderSide(),
+                  ),
+                  //fillColor: Colors.green
+                ),
+                validator: (val) {
+                  if (val.length == 0) {
+                    return "Email cannot be empty";
+                  } else {
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+                style: new TextStyle(
+                  fontFamily: "Poppins",
+                ),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: new InputDecoration(
+                  labelText: "Enter Email",
+                  fillColor: Colors.green,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(25.0),
+                    borderSide: new BorderSide(),
+                  ),
+                  //fillColor: Colors.green
+                ),
+                validator: (val) {
+                  if (val.length == 0) {
+                    return "Email cannot be empty";
+                  } else {
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+                style: new TextStyle(
+                  fontFamily: "Poppins",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
