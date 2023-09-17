bbbe616b4b779356a5218d0d8aaeb42f28a0fb80
prinzpiuz <prinzpiuz@gmail.com>
2023-09-18 00:24:21 +0530

    fix(live details): connection losing in between app is minimised is fixed

--- lib/views/home/real_time_basic_details.dart
+++ lib/views/home/real_time_basic_details.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 // Project imports:
+import 'package:msm/common_widgets.dart';
 import 'package:msm/models/commands/basic_details.dart';
 import 'package:msm/providers/app_provider.dart';
 import 'package:msm/views/home/home_common_widgets.dart';
@@ -33,6 +34,7 @@ class RealTimeBasicDetailsState extends State<RealTimeBasicDetails> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<BasicDetails>(
+        initialData: widget.basicDetails,
         stream: fetchBasicDetailsLive(
             basicDetailsStreamController, widget.appService),
         builder: (
@@ -42,7 +44,7 @@ class RealTimeBasicDetailsState extends State<RealTimeBasicDetails> {
           if (snapshot.hasData) {
             return serverDetails(snapshot.data);
           }
-          return serverDetails(widget.basicDetails);
+          return commonCircularProgressIndicator;
         });
   }
 }
