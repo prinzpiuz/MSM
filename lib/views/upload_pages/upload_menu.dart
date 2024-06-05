ebb9b146898fb2adbc08e2e4ba8b3fb252d53c07
prinzpiuz <prinzpiuz@gmail.com>
2024-06-05 22:31:37 +0530

    refactor(upload page): upload page desing refactor for fixing exit while going back on page issue

--- lib/views/upload_pages/upload_menu.dart
+++ lib/views/upload_pages/upload_menu.dart
@@ -1,12 +1,9 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
-// Package imports:
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-
 // Project imports:
 import 'package:msm/common_utils.dart';
+import 'package:msm/common_widgets.dart';
 import 'package:msm/constants/colors.dart';
 import 'package:msm/router/router_utils.dart';
 import 'package:msm/views/upload_pages/upload_page_utils.dart';
@@ -30,16 +27,12 @@ class UploadMenuPageState extends State<UploadMenuPage> {
 
 Widget uploadMenu(BuildContext context) {
   return Scaffold(
+      appBar: commonAppBar(
+          text: Pages.upload.toTitle,
+          backroute: Pages.home.toPath,
+          context: context),
       backgroundColor: CommonColors.commonWhiteColor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.h),
-          child: StaggeredGrid.count(
-            crossAxisCount: 4,
-            mainAxisSpacing: 4.h,
-            crossAxisSpacing: 4.h,
-            children: tiles(context),
-          ),
-        ),
+      body: ListView(
+        children: locations(context),
       ));
 }
